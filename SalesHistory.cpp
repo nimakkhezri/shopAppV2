93912f295f837fb866aa0318d1810f63f1470db4
NimaKoohkhezri <nimakoohkhezri@gmail.com>
2024-05-04 17:41:46 +0330

    Application appearance has been updated! *some bugs fixed

--- SalesHistory.cpp
+++ SalesHistory.cpp
@@ -6,15 +6,17 @@ void SalesHistory::add_order(Order order) {
 }
 
 void SalesHistory::display_orders(){
-    if(orders.empty())
-        std::cout << "There is no Orders here! Go back and add a new order! " << '\n';
-    else{
+    if(orders.empty()){
+        std::cout << "\n\n\n\t                          ** ORDERS LIST **" << '\n' << '\n';
+        std::cout << "\n\n\tThere is no Orders here! Go back and add a new order! " << '\n';
+        std::cout << "\n\n\t-----------------------------------------------------------------" << '\n';
+    }else{
         int choice;
         do{
             // Showing orders
             display_orders_list();
 
-            std::cout << "Enter order number to view or edit order details (0 for returning back): ";
+            std::cout << "\n\n\tEnter order number to view or edit order details (0 for returning back): ";
             std::cin >> choice;
             if (choice != 0){
                 Order& order = orders[choice - 1];
@@ -57,32 +59,33 @@ void SalesHistory::display_orders(){
 }
 
 void SalesHistory::display_orders_list(){
-    std::cout << std::left << "--------------------" << std::setw(10) << std::internal << "** Orders **" << "--------------------" << '\n';
+    std::cout << "\n\n\n\t                          ** ORDERS LIST **" << '\n' << '\n';
     int i = 1;
         for (Order order : orders){
-            std::cout << i << ". Date: " << order.get_date() << ", Customer name: " << order.get_customer_info().get_full_name() << ", Total price: " << order.get_total_price() << "Toman. \n";
-            i++; 
+            std::cout << "\n\t" << i++ << ". Date: " << order.get_date() << ", Customer name: " << order.get_customer_info().get_full_name() << ", Total price: " << order.get_total_price() << "Toman. \n";
         }
+    std::cout << "\n\n\t-----------------------------------------------------------------" << '\n';
 }
 
 void SalesHistory::veiw_order_detials(int choice){
     Order& order = orders[choice - 1];
-    std::cout << "-------------------- ** Order " << choice <<" Details ** --------------------" << "\n\n";
-    std::cout << "          ---------- ** Customer info ** ----------          " << '\n';
-    std::cout << "Customer Name: " << order.get_customer_info().get_full_name() << '\n';
-    std::cout << "Customer Phone number: " << order.get_customer_info().get_phone_number() << '\n';
-    std::cout << "Date: " << order.get_date() << "\n\n";
-    std::cout << "          ---------- ** Order Products ** ----------          " << '\n';
+
+    std::cout << "\n\n\n\t                         *** ORDER DETAILS ***" << '\n' << '\n';
+    std::cout << "\n\n\t                         ** CUSTOMER INFO **" << '\n' << '\n';
+    std::cout << "\n\tCustomer Name: " << order.get_customer_info().get_full_name() << '\n';
+    std::cout << "\n\tCustomer Phone number: " << order.get_customer_info().get_phone_number() << '\n';
+    std::cout << "\n\tDate: " << order.get_date() << "\n\n";
+    std::cout << "\n\n\t                         ** ORDER PRODUCTS **" << '\n' << '\n';
     int i = 1;
     for(Product product : order.products){
-        std::cout << i <<". " << product.get_name() << '\t' << "Numbers: " << product.get_stock() << '\t' << "base Price: " << product.get_price() << '\t' << "Total Price: " << product.get_price() * product.get_stock() << " Toman" << '\n';
+        std::cout << "\n\t" << i++ <<". " << product.get_name() << '\t' << "Numbers: " << product.get_stock() << '\t' << "base Price: " << product.get_price() << '\t' << "Total Price: " << product.get_price() * product.get_stock() << " Toman" << '\n';
     }
     std::cout << '\n';
-    std::cout << "          ---------- ** Total Price ** ----------          " << '\n';
-    std::cout << "Total Price without discount: " << order.get_total_price() / (1 - order.get_discount()) << '\n';
-    std::cout << "discount Percent: " << order.get_discount() * 100 <<"% \n";
-    std::cout << "Total payment: " << order.get_total_price() << '\n';
-    std::cout << "------------------------------------------------------" << '\n' << '\n';
+    std::cout << "\n\n\t                         ** TOTAL PRICE **" << '\n' << '\n';
+    std::cout << "\n\tTotal Price without discount: " << order.get_total_price() / (1 - order.get_discount()) << '\n';
+    std::cout << "\n\tdiscount Percent: " << order.get_discount() * 100 <<"% \n";
+    std::cout << "\n\tTotal payment: " << order.get_total_price() << '\n';
+    std::cout << "\n\n\t-----------------------------------------------------------------" << '\n';
 }
 
 int SalesHistory::get_total_sales(std::string start_date, std::string end_date) {
--- main.cpp
+++ main.cpp
@@ -21,38 +21,41 @@ int main (){
 
     int choice;
     do {
-        std::cout << "---------------------- Aghche Shop ------------------------" << '\n';
-        std::cout << "1. Add a product " << '\n';
-        std::cout << "2. Products " << '\n';
-        std::cout << "3. Add a new order" << '\n';
-        std::cout << "4. Orders" << '\n';
-        std::cout << "5. Turnover" << '\n';
-        std::cout << "0. Exit" << '\n';
-        std::cout << "---------------------- Aghche Shop ------------------------" << '\n';
-        std::cout << "Enter your choice : ";
+        std::cout << "\n\n\n\t                           ** AGHCHE SHOP **" << '\n';
+        std::cout << "\n\n\t01. NEW PRODUCT                   ---              AFZOODAN-E MAHSOOL .01" << '\n';
+        std::cout << "\n\n\t02. PRODUCTS                      ---              MAHSOOLAT .02" << '\n';
+        std::cout << "\n\n\t03. ADD A NEW ORDER               ---              EJAD-E SEFARESH-E JADID .03" << '\n';
+        std::cout << "\n\n\t04. ORDERS                        ---              SEFARESHAT .04" << '\n';
+        std::cout << "\n\n\t05. TURNOVER                      ---              MODIRIAT-E MALI .05" << '\n';
+        std::cout << "\n\n\t00. EXIT                          ---              KHOROOJ .00" << '\n';
+        std::cout << "\n\n\n\tSelect Your Option (0-5): ";
         std::cin >> choice;
         std::cout << '\n';
 
         switch (choice) {
             case 1:
                 add_product(products);
+                system("CLS");
                 break;
             case 2:
                 int new_choice;
                 display_products(products);
-                std::cout << "For editing products, please enter 1 \n";
-                std::cout << "For returning back please enter 0 \n";
-                std::cout << "Your choise: ";
+                std::cout << "\n\n\t*For editing products, please enter 1 \n";
+                std::cout << "\n\t*For returning back please enter 0 \n";
+                std::cout << "\n\n\tYour choise: ";
                 std::cin >> new_choice;
                 std::cout << '\n';
                 if (new_choice == 1)
                     edit_product(products);
+                system("CLS");
                 break;
             case 3:
                 sales_history.add_order(create_new_order(products));
+                system("CLS");
                 break;
             case 4:
                 sales_history.display_orders();
+                system("CLS");
                 break;
             case 5:
                 break;
@@ -63,94 +66,102 @@ int main (){
 }
 
 void display_products(const std::vector<Product>& products) {
+    std::cout << "\n\n\n\t                          ** PRODUCTS LIST **" << '\n';
+    std::cout << "\n\n\t-----------------------------------------------------------------" << "\n\n";
     if (products.empty()) {
-        std::cout << "There is no product here !!" << std::endl;
+        std::cout << "\n\tThere is no product here !!" << std::endl;
     } else {
-        std::cout << "-------------------- **Products** --------------------" << std::endl;
         int i = 1;
         for (Product product : products) {
-            std::cout << i << ". " << std::setw(20) << std::left << product.get_name() << " (Stock: " << std::setw(5) << product.get_stock() << ", Price: " << std::setw(10) << product.get_price() << "Toman)" << std::endl;
+            std::cout << "\n\t" << i++ << ". " << std::setw(20) << std::left << product.get_name() << " (Stock: " << std::setw(5) << product.get_stock() << ", Price: " << std::setw(10) << product.get_price() << "Toman)" << std::endl;
         }
     }
+    std::cout << "\n\n\t-----------------------------------------------------------------" << '\n';
 }
 
 void edit_product(std::vector<Product>& products) {
-    if (products.empty()) {
-        std::cout << "There is no product here !!" << std::endl;
-        return;
-    }
+    std::cout << "\n\n\n\t                        **** EDIT PRODUCTS ****" << '\n';
 
-    std::cout << "-------------------- **Edit Product** --------------------" << std::endl;
     display_products(products);
 
     int product_index;
-    do {
-        std::cout << "Please enter your product number:  ";
-        std::cin >> product_index;
-    } while (product_index < 1 || product_index > products.size());
-
-    int choice;
-    do {
-        std::cout << "What do you want to do ?" << std::endl;
-        std::cout << "1. Edit stock" << std::endl;
-        std::cout << "2. Edit price" << std::endl;
-        std::cout << "3. Return" << std::endl;
-        std::cout << "Choose: ";
-        std::cin >> choice;
-        std::cout << '\n';
-    } while (choice < 1 || choice > 3);
-
-    Product& product = products[product_index - 1];
-
-    switch (choice) {
-        case 1: {
-        int new_stock;
+    if (products.empty()){
+        system("Pause");
+    }else{
         do {
-            std::cout << "Please enter new stock: ";
-            std::cin >> new_stock;
-        } while (new_stock < 0);
-        product.set_stock(new_stock);
-        std::cout << product.get_name() << "'s stock changed to" << new_stock << ", Succesfully !!" << std::endl;
-        break;
-        }
-        case 2: {
-        double new_price;
+            std::cout << "\n\n\tPlease enter your product number:  ";
+            std::cin >> product_index;
+        } while (product_index < 1 || product_index > products.size());
+
+        int choice;
         do {
-            std::cout << "Please enter new price: ";
-            std::cin >> new_price;
-        } while (new_price < 0);
-        product.set_price(new_price);
-        std::cout << product.get_name() << "'s price changed to" << new_price << " , Succesfully !!" << std::endl;
-        break;
+            std::cout << "\n\n\t*What do you want to do ?" << std::endl;
+            std::cout << "\n\t1. Edit stock" << std::endl;
+            std::cout << "\n\t2. Edit price" << std::endl;
+            std::cout << "\n\t3. Return" << std::endl;
+            std::cout << "\n\tChoose: ";
+            std::cin >> choice;
+            std::cout << '\n';
+        } while (choice < 1 || choice > 3);
+
+        Product& product = products[product_index - 1];
+
+        switch (choice) {
+            case 1: {
+            int new_stock;
+            do {
+                std::cout << "\n\n\tPlease enter new stock: ";
+                std::cin >> new_stock;
+            } while (new_stock < 0);
+            product.set_stock(new_stock);
+            std::cout<< "\n\n\t" << product.get_name() << "'s stock changed to " << new_stock << ", Successfully !!" << std::endl;
+            break;
+            }
+            case 2: {
+            double new_price;
+            do {
+                std::cout << "\n\n\tPlease enter new price: ";
+                std::cin >> new_price;
+            } while (new_price < 0);
+            product.set_price(new_price);
+            std::cout << "\n\n\t" << product.get_name() << "'s price changed to " << new_price << " , Successfully !!" << std::endl;
+            break;
+            }
+            case 3:
+            break;
         }
-        case 3:
-        break;
+        std::cout << "\n\n";
+        system("Pause");
     }
 }
 
 void add_product(std::vector<Product> &products){
+    std::cout << "\n\n\n\t                          ** ADD A NEW PRODUCT **" << '\n';
     std::string product_name;
     int product_price, product_inventory;
-    std::cout << "Please enter the name of your product: ";
+    std::cout << "\n\n\tPlease enter the name of your product: ";
     std::cin.ignore();
     std::getline(std::cin, product_name);
-    std::cout << "Please enter the price of your product: ";
+    std::cout << "\n\n\tPlease enter the price of your product: ";
     std::cin >> product_price;
-    std::cout << "Please enter your product inventory: ";
+    std::cout << "\n\n\tPlease enter your product inventory: ";
     std::cin >> product_inventory;
     products.push_back(Product(product_name, product_inventory, product_price));
-    std::cout << "Your product has been added succesfully!! " << '\n';
+    std::cout << "\n\n\tDone! ... Your product has been added succesfully!!\n\n" << '\n';
+    system("pause");
 }
 
 Order create_new_order(std::vector <Product>& products){
     std::vector<Product> order_products;
+
+    std::cout << "\n\n\n\t                      **** CREAT A NEW ORDER ****" << '\n';
 
     int choice;
     do{
         display_cart(order_products);
         display_products(products);
         
-        std::cout << "Please enter the product number you want(0 for finish choosing and -1 for editing cart) : ";
+        std::cout << "\n\n\tPlease enter the product number you want(0 for finish choosing and -1 for editing cart) : ";
         std::cin >> choice;
 
         if(choice == -1)
@@ -163,11 +174,11 @@ Order create_new_order(std::vector <Product>& products){
 
                 int quantity;
                 do{
-                    std::cout << "You have chose " << product.get_name() << ".... stock = " << product.get_stock() << '\n';
-                    std::cout << "Enter numbers of it : ";
+                    std::cout << "\n\n\t*You have chose " << product.get_name() << "\t stock = " << product.get_stock() << '\n';
+                    std::cout << "\n\n\tEnter numbers of it : ";
                     std::cin >> quantity;
                     if(quantity > product.get_stock() || quantity < 0){
-                        std::cout << "Sorry! Please enter a valid number. \n";
+                        std::cout << "\n\t*Sorry! Please enter a valid number. \n";
                     }
                 }while(quantity < 0 || quantity > product.get_stock());
 
@@ -185,14 +196,14 @@ Order create_new_order(std::vector <Product>& products){
                     product.set_stock(product.get_stock() - quantity);
                 }
                 
-                std::cout << quantity << " number(s) of " << product.get_name() << " have been added to the shopping cart! \n";
+                std::cout << "\n\n\t" << quantity << " number(s) of " << product.get_name() << " have been added to the shopping cart! \n";
 
             }else{
-                std::cout << "There is not enough stock to place an order ... Please try again!" << '\n';
+                std::cout << "\n\t*There is not enough stock to place an order ... Please try again!" << '\n';
             }
         }
         if(choice == 0 && order_products.empty()){
-            std::cout << "Your shopping cart is empty please add some products to you cart and then try again!" << '\n';
+            std::cout << "\n\tYour shopping cart is empty please add some products to you cart and then try again!" << '\n';
         }
     }while(choice != 0 || order_products.empty());
 
@@ -203,7 +214,7 @@ Order create_new_order(std::vector <Product>& products){
     }
 
     double discount_percent; 
-    std::cout << "Please enter the discount amount based on percentage (0 for No-Offer): ";
+    std::cout << "\n\n\tPlease enter the discount amount based on percentage (0 for No-Offer): ";
     std::cin >> discount_percent;
 
     // Current time 
@@ -215,11 +226,11 @@ Order create_new_order(std::vector <Product>& products){
 
     // Customer details
     std::string first_name, last_name, phone_number;
-    std::cout << "Fist name: ";
+    std::cout << "\n\n\tFist name: ";
     std::cin >> first_name;
-    std::cout << "Last name: ";
+    std::cout << "\n\tLast name: ";
     std::cin >> last_name;
-    std::cout << "Phone number: ";
+    std::cout << "\n\tPhone number: ";
     std::cin >> phone_number;
     //std::cout << "Address: ";
     //std::cin.ignore();
@@ -228,59 +239,67 @@ Order create_new_order(std::vector <Product>& products){
   Customer customer(first_name, last_name, phone_number/*, address*/);
 
     Order order(order_products, discount_percent, total_price, customer, current_date);
+    std::cout << "\n\n\tDone!\n";
+    std::cout << "\n\tYour Order has been added successfully!\n\n";
+    system("Pause");
     return order;
 }
 
 void display_cart(const std::vector <Product>& products){
-    std::cout << "-------------------- ** CART ** --------------------" << '\n';
+    std::cout << "\n\n\n\t                          ** YOUR CART **" << "\n\n";
     if(products.empty())
-        std::cout << "Your cart is empty!! " << '\n';
+        std::cout << "\n\tYour cart is empty!! " << '\n';
     else{
         int i = 1;
         for(Product product : products){
-            std::cout << i << ". " << std::left << std::setw(120) << product.get_name() << " Quantitiy: " << std::setw(5) << product.get_stock() << '\n';
+            std::cout << "\n\t" << i << ". " << std::left << std::setw(20) << product.get_name() << " Quantitiy: " << std::setw(5) << product.get_stock() << '\n';
             i++;
         }
     }
-    std::cout << "-------------------- ** CART ** --------------------" << '\n' << '\n';
+    std::cout << "\n\n\t-----------------------------------------------------------------" << '\n' << '\n';
 }
 
 void edit_cart(std::vector <Product>& order_products, std::vector <Product>& products){
-    std::cout << "-------------------- ** EDIT CART ** --------------------" << '\n' << '\n';
+    std::cout << "\n\n\n\t                           ** YOUR CART **" << "\n\n";
     int i = 1;
-    for (Product product : order_products){
-        std::cout << i << ". " << std::left << std::setw(20) << product.get_name() << " Quantity: " << std::setw(5) << product.get_stock() << '\n';
-    }
-    std::cout << '\n';
-    std::cout << "Please enter a product number to edit (0 for returning back): ";
-    int choice; 
-    std::cin >> choice;
-    int product_total_stock;
-
-    if (choice > 0 && choice <= order_products.size()){
-        Product& order_product = order_products[choice - 1];
-
-        for(Product& product : products){
-            if(order_product.get_name() == product.get_name())
-                product_total_stock = product.get_stock() + order_product.get_stock();
+    if(order_products.empty()){
+        std::cout << "\n\tYour cart is empty!! " << '\n';
+    }else{
+        for (Product product : order_products){
+            std::cout << "\n\t" << i++ << ". " << std::left << std::setw(20) << product.get_name() << " Quantity: " << std::setw(5) << product.get_stock() << '\n';
         }
-        int new_quantity;
+        std::cout << "\n\n\t-----------------------------------------------------------------" << '\n' << '\n';
 
-        do{
-            std::cout << "You have chosen " << std::left << std::setw(20) << order_product.get_name() << " Quantity: " << std::setw(5) << order_product.get_stock() << '\n';
-            std::cout << "Please enter a new quantity: ";
-            std::cin >> new_quantity;
-            if(new_quantity < 0 || new_quantity > product_total_stock)
-                std::cout << "Please enter a valid value! " << '\n';
-        }while(new_quantity < 0 || new_quantity > product_total_stock);
+        std::cout << "\n\n\t*Please enter a product number to edit (0 for returning back): ";
+        int choice; 
+        std::cin >> choice;
+        int product_total_stock;
 
-        order_product.set_stock(new_quantity);
+        if (choice > 0 && choice <= order_products.size()){
+            Product& order_product = order_products[choice - 1];
 
-        for(Product& product : products){
-            if(order_product.get_name() == product.get_name())
-                product.set_stock(product_total_stock - new_quantity);
+            for(Product& product : products){
+                if(order_product.get_name() == product.get_name())
+                    product_total_stock = product.get_stock() + order_product.get_stock();
+            }
+            int new_quantity;
+
+            do{
+                std::cout << "\n\n\t*You have chosen " << std::left << std::setw(20) << order_product.get_name() << " Quantity: " << std::setw(5) << order_product.get_stock() << '\n';
+                std::cout << "\n\n\tPlease enter a new quantity: ";
+                std::cin >> new_quantity;
+                if(new_quantity < 0 || new_quantity > product_total_stock)
+                    std::cout << "\n\n\tPlease enter a valid value! " << '\n';
+            }while(new_quantity < 0 || new_quantity > product_total_stock);
+
+            order_product.set_stock(new_quantity);
+
+            for(Product& product : products){
+                if(order_product.get_name() == product.get_name())
+                    product.set_stock(product_total_stock - new_quantity);
+            }
+        std::cout << "\n\n\tDone!" << '\n';
         }
-    std::cout << "Done!" << '\n';
     }
 }
 
